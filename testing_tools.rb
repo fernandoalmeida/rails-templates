4655cf004ecbf01e0e760c5564bbea91c7c19638
Fernando Almeida <fernando@fernandoalmeida.net>
2013-10-07 00:52:17 -0300

    New template: Testing tools

---
+++ testing_tools.rb
@@ -0,0 +1,116 @@
+gem_group :development, :test do
+
+  gem 'database_cleaner'
+
+  gem 'rspec-rails'
+  gem 'guard-rails'
+  gem 'guard-rspec'
+
+  gem 'spork-rails', github: 'sporkrb/spork-rails'
+  gem 'guard-spork'
+
+  gem 'brakeman', :require => false
+  gem 'guard-brakeman'
+
+  gem 'shoulda-matchers'
+  gem 'valid_attribute'
+  gem 'factory_girl_rails'
+
+  gem 'webrat'
+  gem 'launchy'
+end
+
+file ".rspec", <<-CODE
+--color
+--drb
+--format nested
+--profile
+CODE
+
+file "spec/spec_helper.rb", <<'CODE'
+require 'rubygems'
+require 'spork'
+
+ENV["RAILS_ENV"] ||= 'test'
+
+Spork.prefork do
+
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'rspec/autorun'
+
+  Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+  ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+
+  RSpec.configure do |config|
+
+    config.mock_with :rspec
+    config.fixture_path = "#{::Rails.root}/spec/fixtures"
+    config.use_transactional_fixtures = false
+    config.infer_base_class_for_anonymous_controllers = false
+    config.order = "random"
+
+    config.before(:suite) do
+      DatabaseCleaner.strategy = :transaction
+      # DatabaseCleaner.clean_with(:truncation)
+    end
+
+    config.before(:each) do
+      DatabaseCleaner.start
+    end
+
+    config.after(:each) do
+      DatabaseCleaner.clean
+    end
+
+  end
+end
+
+Spork.each_run do
+
+end
+CODE
+
+file "Guardfile", <<'CODE'
+guard 'spork', :rspec_env => { 'RAILS_ENV' => 'test' } do
+  watch('config/application.rb')
+  watch('config/environment.rb')
+  watch(%r{^config/environments/.+\.rb$})
+  watch(%r{^config/initializers/.+\.rb$})
+  watch('spec/spec_helper.rb')
+end
+
+rspec_options = {
+  :test_unit => false,
+  :all_after_pass => false,
+  :all_on_start => false
+}
+
+guard :rspec, rspec_options do
+  # Rspec
+  watch(%r{^spec/.+_spec\.rb$})
+  watch(%r{^lib/(.+)\.rb$})     { |m| "spec/lib/#{m[1]}_spec.rb" }
+  watch('spec/spec_helper.rb')  { "spec" }
+
+  # Rails
+  watch(%r{^app/(.+)\.rb$})                           { |m| "spec/#{m[1]}_spec.rb" }
+  watch(%r{^app/(.*)(\.erb|\.haml)$})                 { |m| "spec/#{m[1]}#{m[2]}_spec.rb" }
+  watch(%r{^app/controllers/(.+)_(controller)\.rb$})  { |m| ["spec/routing/#{m[1]}_routing_spec.rb", "spec/#{m[2]}s/#{m[1]}_#{m[2]}_spec.rb", "spec/acceptance/#{m[1]}_spec.rb"] }
+  watch(%r{^spec/support/(.+)\.rb$})                  { "spec" }
+  watch('config/routes.rb')                           { "spec/routing" }
+  watch('app/controllers/application_controller.rb')  { "spec/controllers" }
+end
+
+guard 'rails' do
+  watch('Gemfile.lock')
+  watch(%r{^(config|lib)/.*})
+end
+
+guard 'brakeman' do
+  watch(%r{^app/.+\.(erb|haml|rhtml|rb)$})
+  watch(%r{^config/.+\.rb$})
+  watch(%r{^lib/.+\.rb$})
+  watch('Gemfile')
+end
+CODE
